0644153afb728a8c028a84f7415e1e28c5546030
joelsc <joelsc1597@gmail.com>
2024-01-01 14:24:10 -0500

    configuration vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://joelsc1597@gmail.com.github.io/sisuni-webpage"
 })
