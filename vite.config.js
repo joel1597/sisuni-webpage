b3fc99b7dfd55207437f36bdf2ce149859d4753c
joelsc <joelsc1597@gmail.com>
2024-01-01 15:36:31 -0500

    fix vite-config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://joelsc1597.github.io/sisuni-webpage"
+  base: "https://joel1597.github.io/Crud-React-Ant-Design"
 })
